d329693bfe4124f92cceff07b1fe10c0fd53201a
Adam Linkhart <alinkhart@gmail.com>
2019-03-08 14:31:24 -0500

    created Card model class

---
+++ MemoryGame/Models/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  MemoryGame
+//
+//  Created by alinkhart on 3/8/19.
+//  Copyright © 2019 Adam Linkhart, LLC. All rights reserved.
+//
+
+import Foundation
+
+class Card {
+    
+}
