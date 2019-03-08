bd9c193069fca85383b87650d84e6fb5df509e80
Adam Linkhart <alinkhart@gmail.com>
2019-03-08 14:36:45 -0500

    implemented Card model class

--- MemoryGame/Models/Card.swift
+++ MemoryGame/Models/Card.swift
@@ -6,8 +6,22 @@
 //  Copyright © 2019 Adam Linkhart, LLC. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class Card {
     
+    var name: String
+    var frontImage: UIImage
+    var isFlipped = false
+    var isMatched = false
+    let backImage = UIImage(named: "CardBack")
+    
+    init (name: String, frontImage: UIImage) {
+        self.name = name
+        self.frontImage = frontImage
+    }
+    
+    func matches(card: Card) -> Bool {
+        return name == card.name
+    }
 }
