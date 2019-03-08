912db18c64fe703bc60eb2941c9ec61b57fa1d9e
Adam Linkhart <alinkhart@gmail.com>
2019-03-08 13:46:40 -0500

    delegate Lobby view button taps to its delegate

--- MemoryGame/ViewControllers/LobbyViewController.swift
+++ MemoryGame/ViewControllers/LobbyViewController.swift
@@ -13,21 +13,26 @@ protocol LobbyViewControllerDelegate: class {
 }
 
 final class LobbyViewController: UIViewController, StoryboardInitializable {
+    
+    weak var delegate: LobbyViewControllerDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func threeByFourButtonTapped(_ sender: Any) {
+        delegate?.startGameButtonTapped(columns: 3, rows: 4)
     }
     
     @IBAction func fiveByTwoButtonTapped(_ sender: Any) {
+        delegate?.startGameButtonTapped(columns: 5, rows: 2)
     }
     
     @IBAction func fourByFourButtonTapped(_ sender: Any) {
+        delegate?.startGameButtonTapped(columns: 4, rows: 4)
     }
     
     @IBAction func fourByFiveButtonTapped(_ sender: Any) {
+        delegate?.startGameButtonTapped(columns: 4, rows: 5)
     }
-    
 }
