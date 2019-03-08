1afc95845c49c34c3ab9a46cf820f5ba9152e310
Adam Linkhart <alinkhart@gmail.com>
2019-03-08 13:25:33 -0500

    implemented StoryboardInitializable

--- MemoryGame/Helpers/StoryboardInitializable.swift
+++ MemoryGame/Helpers/StoryboardInitializable.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2019 Adam Linkhart, LLC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol StoryboardInitializable {
+    static var storyboardName: String { get }
+    static var storyboardBundle: Bundle? { get }
+    
+    static func makeFromStoryboard() -> Self
+}
+
+extension StoryboardInitializable where Self : UIViewController {
+    static var storyboardName: String {
+        return "Main"
+    }
+    
+    static var storyboardBundle: Bundle? {
+        return nil
+    }
+    
+    static var storyboardIdentifier: String {
+        return String(describing: self)
+    }
+    
+    static func makeFromStoryboard() -> Self {
+        let storyboard = UIStoryboard(name: storyboardName, bundle: storyboardBundle)
+        return storyboard.instantiateViewController(withIdentifier: storyboardIdentifier) as! Self
+    }
+}
