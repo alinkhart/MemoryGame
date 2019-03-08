79351949a6ad4693af30247534ea47ab043b9d14
Adam Linkhart <alinkhart@gmail.com>
2019-03-08 13:32:09 -0500

    created LobbyCoordinator

---
+++ MemoryGame/Coordinators/LobbyCoordinator.swift
@@ -0,0 +1,13 @@
+//
+//  LobbyCoordinator.swift
+//  MemoryGame
+//
+//  Created by alinkhart on 3/8/19.
+//  Copyright © 2019 Adam Linkhart, LLC. All rights reserved.
+//
+
+import Foundation
+
+class LobbyCoordinator {
+    
+}
