c945379a5188c4250ac7f497230aa58fca5baf6a
Adam Linkhart <alinkhart@gmail.com>
2019-03-08 14:49:19 -0500

    beginning of Game model

--- MemoryGame/Models/Game.swift
+++ MemoryGame/Models/Game.swift
@@ -6,8 +6,18 @@
 //  Copyright © 2019 Adam Linkhart, LLC. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class Game {
     
+    var columns: Int
+    var rows: Int
+    var cards: [Card]?
+    
+    private var cardNames = ["Bat", "Cat", "Cow", "Dragon", "GarbageMan", "GhostDog", "Hen", "Horse", "Pig", "Spider"]
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+    }
 }
