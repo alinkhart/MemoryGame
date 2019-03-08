b4c073f40cf89c537dbf957c0fd5f05a37334928
Adam Linkhart <alinkhart@gmail.com>
2019-03-08 13:30:36 -0500

    created Coordinator protocol

---
+++ MemoryGame/Coordinators/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  MemoryGame
+//
+//  Created by alinkhart on 3/8/19.
+//  Copyright © 2019 Adam Linkhart, LLC. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinator {
+    func start(animated: Bool)
+}
